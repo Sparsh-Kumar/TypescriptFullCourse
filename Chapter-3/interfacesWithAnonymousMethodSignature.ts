5c75a7f678f44002c70b21af11e80408ae5d6c49
Sparsh Kumar <sparshkumar@Sparshs-MacBook-Pro.local>
2021-12-25 11:33:09 +0530

    [interfaces] with anonymous Method signatures.

---
+++ Chapter-3/interfacesWithAnonymousMethodSignature.ts
@@ -0,0 +1,29 @@
+
+
+/**
+ * An interface with an anonymous method signature describes a function.
+ * But a function in the JavaScript realm is also an object, which means you can add properties to a function value just like an object.
+ * Therefore it is perfectly legal you can define any properties on an interface of the function type.
+ */
+
+interface IsSumOddCheck {
+    (a: number, b: number): boolean;
+    type: string;
+    calculate (a: number, b: number): number;
+}
+
+const IsSumOddCheck: IsSumOddCheck = Object.assign (
+    function (numOne: number, numTwo: number) : boolean {
+        return (((numOne + numTwo) % 2) !== 0)
+    },
+    {
+        type: 'Odd Check Function',
+        calculate: (a: number, b: number): number => {
+            return a + b;
+        }
+    }
+)
+
+console.log ('IsSumOddCheck(1, 2) => ', IsSumOddCheck(3, 4));
+console.log (IsSumOddCheck.type);
+console.log (IsSumOddCheck.calculate(2, 3));
